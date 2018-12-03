55ed4e1ec4acc8a7511d195c37f4d5eee0127b38
Allen-Lab <420703660@qq.com>
2018-12-03 11:00:13 +0800

    add status bar

--- tk.py
+++ tk.py
@@ -2,9 +2,11 @@ from tkinter import *
 from tkinter import filedialog
 from PIL import Image, ImageTk
 import glob
+import os
 
 
 class Window(Frame):
+
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
@@ -15,6 +17,7 @@ class Window(Frame):
         self.img_list = []  # list of image paths.
         self.cur_img = 0    # index of current showing image.
     
+
     # Creation of init_window
     def init_window(self):
 
@@ -41,18 +44,26 @@ class Window(Frame):
 
         rightButton = Button(self.master, text="next image", command=self.next_image)
         rightButton.grid(row=0, column=2)
+        
+        # creating a status bar instance
+        self.status = StringVar()
+        statusBar = Label(self.master, anchor=W, textvariable=self.status)
+        self.status.set('Status Bar')
+        statusBar.grid(columnspan=3, sticky=E)
 
 
     def prev_image(self):
         if(self.cur_img > 0):
             self.cur_img -= 1
-        self.showImg(self.img_list[self.cur_img])
+        self.showImg()
+        self.update_status()
 
 
     def next_image(self):
         if(self.cur_img < len(self.img_list)):
             self.cur_img += 1
-        self.showImg(self.img_list[self.cur_img])
+        self.showImg()
+        self.update_status()
 
 
     # open images directory and show first image.
@@ -62,18 +73,21 @@ class Window(Frame):
         self.img_list += glob.glob(imgs_path+'/*.PNG')
         self.img_list += glob.glob(imgs_path+'/*.jpg')
         self.img_list += glob.glob(imgs_path+'/*.JPG')
-        self.showImg(self.img_list[self.cur_img])
+        self.showImg()
+        self.update_status()
 
 
+    def update_status(self):
+        basename, filename = os.path.split(self.img_list[self.cur_img])
+        self.status.set(filename)
 
-    def showImg(self, img_path):
-        print(img_path)
+
+    def showImg(self):
+        img_path = self.img_list[self.cur_img]
         load = Image.open(img_path)
         render = ImageTk.PhotoImage(load)
-        # labels can be text or images
         img = Label(self.master, image=render)
         img.image = render
-        # img.place(x=0, y=0)
         img.grid(row=0, column=1)
 
 
